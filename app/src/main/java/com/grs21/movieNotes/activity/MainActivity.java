327b241d976450769352ea27648dbd5816ada219
Fatih Gürses <fatihgurses@gmail.com>
2020-11-21 10:31:14 +0300

    movieNotes

--- app/src/main/java/com/grs21/movieNotes/activity/MainActivity.java
+++ app/src/main/java/com/grs21/movieNotes/activity/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerAdapter recyclerAdapter=new RecyclerAdapter(movieName);
         recyclerView.setAdapter(recyclerAdapter);
-        
+
     }
 
     public void initializeComponent(){
